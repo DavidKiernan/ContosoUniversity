2ebf0266fc31dcd9d16e871c738b097711684b43
david kiernan <davidkiernan@hotmail.co.uk>
2018-01-06 11:49:15 +0000

    Tutorial Complete, Getting Started with Entity Framework

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,18 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; } // PK uses "classname" ID pattern
+        public int CourseID { get; set; } // FK
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; } // The "?" indicates Grade propery is nullable ( isn't known or hasn't been assigned yet)
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; } // PK
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; } // Nav Property, hold other entities that are related to this entity.
+                                                                         // Nav properties are typically defined as virtual so that they can take advantage of certain EF functionality such as lazy loading. 
+    }
+}
